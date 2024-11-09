e4bd46902fc471bfc35552b578f54d7ccdbe6d61
Artem <artem.kotyakhov@gmail.com>
2024-11-09 14:53:34 +0300

    add email validation

--- BudgetTracker/budgettracker/authentication/views.py
+++ BudgetTracker/budgettracker/authentication/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 
 import json
+from validate_email import validate_email
 
 
 class UsernameValidationView(View):
@@ -18,6 +19,19 @@ class UsernameValidationView(View):
             return JsonResponse({'username_error': 'sorry, username already using, choose another one'},
                                 status=409)
         return JsonResponse({'username_valid': True})
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email not valid'},
+                                status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'sorry, email already using, choose another one'},
+                                status=409)
+        return JsonResponse({'email_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
--- BudgetTracker/budgettracker/static/js/register.js
+++ BudgetTracker/budgettracker/static/js/register.js
@@ -1,8 +1,10 @@
 const usernameField = document.querySelector('#usernameField');
 const feedBackArea = document.querySelector('.invalid-feedback');
+const emailField = document.querySelector('#emailField');
+const emailfeedBackArea = document.querySelector('.emailfeedBackArea');
+
 
 usernameField.addEventListener("keyup", (e) => {
-    console.log('777', 777);
     const usernameVal=e.target.value;
 
     usernameField.classList.remove('is-invalid');
@@ -20,6 +22,31 @@ usernameField.addEventListener("keyup", (e) => {
                 usernameField.classList.add('is-invalid');
                 feedBackArea.style.display = 'block';
                 feedBackArea.innerHTML=`<p>${data.username_error}</p>`;
+            }
+        });
+    }
+
+    });
+
+
+emailField.addEventListener("keyup", (e) => {
+    const emailVal=e.target.value;
+
+    emailField.classList.remove('is-invalid');
+    emailfeedBackArea.style.display = 'none';
+
+    if(emailVal.length>0){
+        fetch('/auth/validate-email/',{
+        body: JSON.stringify({email: emailVal}),
+        method: 'POST',
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log('data', data);
+            if(data.email_error){
+                emailField.classList.add('is-invalid');
+                emailfeedBackArea.style.display = 'block';
+                emailfeedBackArea.innerHTML=`<p>${data.email_error}</p>`;
             }
         });
     }
